33fc5f303f27864603ab71b32f102fb9eac8ddcc
partolaaa <ivan.partola77@gmail.com>
2024-03-19 16:46:48 +0100

    Created project

---
+++ src/main/java/com/nure/lazure/LazureApplication.java
@@ -0,0 +1,13 @@
+package com.nure.lazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazureApplication.class, args);
+    }
+
+}
